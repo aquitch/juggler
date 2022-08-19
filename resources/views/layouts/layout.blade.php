3e4c5c74c8522ac174210f49c96185e13ee6f3ff
aquitch <aquitch@gmail.com>
2022-08-19 20:37:13 +0400

    livewire

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,12 +7,14 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+@livewireStyles
 <!-- Own style -->
 <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 
 <body>
     @include('includes.header')
     @yield('content')
+    @livewireScripts
 </body>
 <script type="text/javascript" src="{{ asset('js/theme.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/appendForm.js') }}"></script>
